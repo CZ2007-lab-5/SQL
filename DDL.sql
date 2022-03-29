ac87f1fa4e8b2f6b0e153ec90b762e3c0d3be968
BotBw <wang1570@e.ntu.edu.sg>
2022-03-30 01:14:42 +0800

    Capitalize

--- DDL.sql
+++ DDL.sql
@@ -56,7 +56,7 @@ create table COMPLAINTS (
   Filed_date_time datetime not null,
   Handled_date_time datetime,
   Addressed_date_time datetime,
-  Status varchar(20) check (Status in ('pending', 'being handled', 'addressed')),
+  Status varchar(20) check (Status in ('Pending', 'Being handled', 'Addressed')),
   primary key (ID),
   foreign key (UID) references USERS(UID) on update cascade on delete set null,
   foreign key (EID) references EMPLOYEES(ID) on update cascade on delete set null
